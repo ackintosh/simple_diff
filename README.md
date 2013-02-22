85c58909f387556e9f2dd022bba12a0e60b11806
Akihito Nakano <sora.akatsuki@gmail.com>
2013-02-22 10:38:45 +0900

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 simple_diff
 ===========
+##2つのテキストファイルに共通する行を抽出する
+
+    $ ruby file_diff.rb hoge.txt fuga.txt
---
+++ file_diff.rb
@@ -0,0 +1,39 @@
+# -*- encoding: utf-8 -*-
+# 2つのテキストファイルに共通する行を抽出する
+
+def get_rows(filename)
+	res = []
+	begin
+		open(filename) do |file|
+			file.each do |row|
+				res.push row.chomp
+			end
+		end
+	rescue
+		puts "file can't open."
+	end
+	res
+end
+
+def compare(names, target_filename)
+	begin
+		open(target_filename) do |file|
+			file.each do |row|
+				puts row if names.include? row.chomp
+			end
+		end
+	rescue
+		puts "file can't open."
+	end
+end
+
+def diff(old_filename, new_filename)
+	old_rows = get_rows(old_filename)
+	compare(old_rows, new_filename)
+end
+
+
+old_filename = $*[0]
+new_filename = $*[1]
+
+diff(old_filename, new_filename)
